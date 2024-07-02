b286f82e42c3addf57affde3406cb642e95a0f0f
aritrag281 <aritrag281@gmail.com>
2024-07-02 22:21:00 +0530

    static changed again

--- disease_prediction/disease_prediction/settings.py
+++ disease_prediction/disease_prediction/settings.py
@@ -131,5 +131,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'templates')
 ]
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
