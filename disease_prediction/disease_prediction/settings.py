42f365d333457fc12769f81d53e0cab372e4ef58
aritrag281 <aritrag281@gmail.com>
2024-07-02 22:12:04 +0530

    static revert

--- disease_prediction/disease_prediction/settings.py
+++ disease_prediction/disease_prediction/settings.py
@@ -130,3 +130,6 @@ STATICFILES_DIRS = [
 
     os.path.join(BASE_DIR,'templates')
 ]
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
--- disease_prediction/disease_prediction/urls.py
+++ disease_prediction/disease_prediction/urls.py
@@ -24,3 +24,6 @@ urlpatterns = [
      path("accounts/", include("accounts.urls")),
      path("", include("chats.urls"))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
